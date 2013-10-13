75d9c492bf7bdb13b15ec48a5e0eae9d74c44208
papi92 <afox1208@gmail.com>
2013-10-13 00:43:20 -0400

    Used by buildbox to automate all builds.
    Change-Id: Iee5c6a8d3c9f3b7114c94984f5b2e42bdfab86d2

---
+++ build-all.sh
@@ -0,0 +1,1 @@
+repo sync -j126 && make clean && . build/envsetup.sh && brunch mako -j126 && . build/envsetup.sh && brunch m7ul -j126 && . build/envsetup.sh && brunch jflteatt -j126 && . build/envsetup.sh && brunch jfltevzw -j126 && . build/envsetup.sh && brunch jfltetmo -j126 && . build/envsetup.sh && brunch jfltespr -j126
